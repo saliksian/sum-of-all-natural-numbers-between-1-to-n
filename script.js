f93307aa2f8242f18ac3ddffaf7b40f0a376f663
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2021-04-20 11:35:29 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let number = 100; // here you can take this number from user by using prompt method
+let sum = number * (number+1)/2;  // I got this algo from PTB class 9th computer science book first chapter
+
+console.log(sum);
